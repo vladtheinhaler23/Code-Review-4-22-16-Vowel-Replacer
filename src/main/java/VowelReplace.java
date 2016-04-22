96517d4300a9ce8249d0222108796edbb568bb2d
Joshua Logan <joshlogan32323@gmail.com>
2016-04-21 20:32:24 -0700

    intital commit with basic file structure to begin testing basic logic

---
+++ src/main/java/VowelReplace.java
@@ -0,0 +1,8 @@
+public class VowelReplace {
+  public static void main(String[] args) {}
+
+  public String vowelReplacer(String userInput) {
+    return "";
+  }
+
+}
---
+++ src/test/java/VowelReplaceTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class VowelReplaceTest {
+
+  @Test
+  public void vowelReplacer_forASingleConsonant_b() {
+    // eventually put your testing code here
+  }
+}
